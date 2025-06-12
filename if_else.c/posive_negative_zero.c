6a4fa27bcd89e92292d0c503721f1064bfd4d2de
RitamMondal2468 <ritammondal700144@gmail.com>
2025-06-12 22:41:36 +0530

    Added now code

---
+++ if_else.c/posive_negative_zero.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+  int num;
+
+  printf("Enter the number: ");
+  scanf("%d", &num);
+
+  if (num > 0)
+  {
+    printf("The number %d is positive\n", num);
+  }
+  else if (num < 0)
+  {
+    printf("The number %d is negative\n", num);
+  }
+  else
+  {
+    printf("The number is zero\n");
+  }
+  return 0;
+}
